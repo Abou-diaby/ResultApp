a40c1e88ba082a821bfd3649644b885ec0f8fca1
Papelenoir <sinediaby@gmail.com>
2024-09-24 17:15:06 +0000

    Mise en place de la scène
    -Ajout du css
    -chargement de l'icone
    -Gestion d'exception

--- src/ci/exam/ResultApp.java
+++ src/ci/exam/ResultApp.java
@@ -1,6 +1,12 @@
 package ci.exam;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ResultApp extends Application {
@@ -8,8 +14,27 @@ public class ResultApp extends Application {
 	@SuppressWarnings("exports")
 	@Override
 	public void start(Stage stage) throws Exception {
-		
-		
+		try {
+			Parent root = FXMLLoader.load(FXMLPage.HOME.getPage());
+
+			Scene scene = new Scene(root);
+			// Ajout du fichier CSS
+			scene.getStylesheets().add("styles.css");
+			
+			// Charger l'icône
+			stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/icon.png")));
+			
+			stage.setScene(scene);
+			stage.setTitle("Résultat de l'examen national");
+			stage.setResizable(false);
+			stage.show();
+			
+		} catch (IOException e) {
+			e.printStackTrace(); // Affichage de l'erreur dans la console
+		}
 	}
 	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
