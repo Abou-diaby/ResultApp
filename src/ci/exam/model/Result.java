e9ec03036fee5f8805c29b3acf472b05d4f0db0c
Papelenoir <sinediaby@gmail.com>
2024-09-24 18:44:49 +0000

    Création du modèle Result(Résultat)
    Déclaration des variables
    Constructeur avec tous les paramètres
    Getters

---
+++ src/ci/exam/model/Result.java
@@ -0,0 +1,59 @@
+package ci.exam.model;
+
+import java.util.Date;
+
+public class Result {
+    private int id;
+    private String matricule;
+    private boolean statut;
+    private float moyenne;
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+    private String ecole;
+
+    // Constructeur avec tous les paramètres
+    public Result(int id, String matricule, boolean statut, float moyenne, String nom, String prenom, Date dateNaissance, String ecole) {
+        this.id = id;
+        this.matricule = matricule;
+        this.statut = statut;
+        this.moyenne = moyenne;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.ecole = ecole;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public boolean getStatut() {
+        return statut;
+    }
+
+    public float getMoyenne() {
+        return moyenne;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public String getEcole() {
+        return ecole;
+    }
+}
